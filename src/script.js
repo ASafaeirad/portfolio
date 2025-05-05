27a11f01ad801e2308d49b8c046787277b9ee70d
Alireza Safaierad <frontendmonster@gmail.com>
2025-05-05 19:16:11 +0200

    refactor parallax

--- src/script.js
+++ src/script.js
@@ -2,15 +2,19 @@ import { clamp } from '@fullstacksjs/toolbox';
 
 import './styles.css';
 
+function setParallax({ x, y, blur }) {
+  document.body.style.setProperty('--pointer-x', `${x / 2}px`);
+  document.body.style.setProperty('--pointer-y', `${y / 4}px`);
+  document.body.style.setProperty('--blur-man', `${blur * -1}px`);
+  document.body.style.setProperty('--blur-deer', `${blur}px`);
+}
+
 document.addEventListener('mousemove', (e) => {
   const fromCenterX = Math.round((e.clientX / window.innerWidth) * 100) - 50;
   const fromCenterY = Math.round((e.clientY / window.innerHeight) * 100) - 50;
   const blur = Math.min((fromCenterX + fromCenterY) / 10, 10);
 
-  document.body.style.setProperty('--pointer-x', `${fromCenterX / 2}px`);
-  document.body.style.setProperty('--pointer-y', `${fromCenterY / 4}px`);
-  document.body.style.setProperty('--blur-man', `${blur * -1}px`);
-  document.body.style.setProperty('--blur-deer', `${blur}px`);
+  setParallax({ x: fromCenterX, y: fromCenterY, blur });
 });
 
 let accX = 0;
@@ -23,8 +27,5 @@ window.addEventListener('devicemotion', (event) => {
   accY = clamp(accY + rotation.alpha / 100, -50, 50);
   const blur = Math.min((accX + accY) / 5, 10);
 
-  document.body.style.setProperty('--pointer-x', `${accX}px`);
-  document.body.style.setProperty('--pointer-y', `${accY}px`);
-  document.body.style.setProperty('--blur-man', `${blur * -1}px`);
-  document.body.style.setProperty('--blur-deer', `${blur}px`);
+  setParallax({ x: accX, y: accY, blur });
 });
