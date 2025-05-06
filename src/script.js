470f3599c2e8536e27cd05e163dd66f2ffb7d1aa
Alireza Safaierad <frontendmonster@gmail.com>
2025-05-07 01:09:20 +0200

    fix mobile mousemove

--- src/script.js
+++ src/script.js
@@ -2,6 +2,10 @@ import { clamp } from '@fullstacksjs/toolbox';
 
 import './styles.css';
 
+let accX = 0;
+let accY = 0;
+let hasMotionDetected = false;
+
 function setParallax({ x, y, blur }) {
   document.body.style.setProperty('--pointer-x', `${x / 2}px`);
   document.body.style.setProperty('--pointer-y', `${y / 4}px`);
@@ -9,7 +13,9 @@ function setParallax({ x, y, blur }) {
   document.body.style.setProperty('--blur-deer', `${blur}px`);
 }
 
-document.addEventListener('mousemove', (e) => {
+window.addEventListener('mousemove', (e) => {
+  if (hasMotionDetected) return;
+
   const fromCenterX = Math.round((e.clientX / window.innerWidth) * 100) - 50;
   const fromCenterY = Math.round((e.clientY / window.innerHeight) * 100) - 50;
   const blur = Math.min((fromCenterX + fromCenterY) / 10, 10);
@@ -17,15 +23,15 @@ document.addEventListener('mousemove', (e) => {
   setParallax({ x: fromCenterX, y: fromCenterY, blur });
 });
 
-let accX = 0;
-let accY = 0;
-
 window.addEventListener('devicemotion', (event) => {
   const rotation = event.rotationRate;
 
   accX = clamp(accX + rotation.beta / 100, -50, 50);
   accY = clamp(accY + rotation.alpha / 100, -50, 50);
   const blur = Math.min((accX + accY) / 5, 10);
+  if (accX !== 0 || accY !== 0) {
+    hasMotionDetected = true;
+  }
 
   setParallax({ x: accX, y: accY, blur });
 });
